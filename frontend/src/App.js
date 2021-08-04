2a472790b0c7177dc2a9b25ba042f4fc90c9e41a
Groofi <varchikcreed@mail.ru>
2021-08-04 05:33:21 +0300

    Home_7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import axios from 'axios'
 import UserList from './User'
-import MenuList from "./Menu";
 import FooterList from "./Footer";
 import ProjectList from "./components/Projects";
 import TodoList from "./components/Todo";
-import {HashRouter, Route, Switch} from "react-router-dom";
+import Cookies from 'universal-cookie';
+import {HashRouter, Link, Route, Switch} from "react-router-dom";
+import LoginForm from "./components/LoginForm";
 
 const NotFound404 = ({location}) => {
     return (
@@ -24,33 +25,143 @@ class App extends React.Component {
             'users': [],
             'projects': [],
             'todos': [],
+            'token': '',
         }
     }
 
+    is_auth() {
+        return !!this.state.token
+    }
+
+    get_token_from_storage() {
+        const cookie = new Cookies()
+        this.setState({'token': cookie.get('token')}, this.get_data)
+    }
+
+    get_headers() {
+        let header = {
+            'Content-Type': 'application/json'
+        }
+        const cookie = new Cookies()
+
+        header['Authorization'] = 'Token ' + cookie.get('token')
+
+        return header;
+    }
+
+    get_data() {
+        const headers = this.get_headers()
+
+        axios.get('http://127.0.0.1:8000/api/users/', {headers})
+            .then(
+                response => {
+                    const users = response.data.results
+                    this.setState({
+                        'users': users
+                    })
+                }
+            ).catch(
+            error => {
+                this.setState({
+                    'users': []
+                })
+                console.log(error)
+            }
+        )
+
+        axios.get('http://127.0.0.1:8000/api/project/', {headers})
+            .then(
+                response => {
+                    const projects = response.data.results
+                    this.setState({
+                        'projects': projects
+                    })
+                }
+            ).catch(
+            error => {
+                this.setState({
+                    'projects': []
+                })
+                console.log(error)
+            }
+        )
+
+        axios.get('http://127.0.0.1:8000/api/todo/', {headers})
+            .then(
+                response => {
+                    const todos = response.data.results
+                    this.setState({
+                        'todos': todos
+                    })
+                }
+            ).catch(
+            error => {
+                this.setState({
+                    'todos': []
+                })
+                console.log(error)
+            }
+        )
+    }
+
+    get_token(login, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/',
+            {
+                "username": login,
+                "password": password
+            })
+            .then(
+                response => {
+                    const cookie = new Cookies()
+                    cookie.set('token', response.data.token)
+                    this.setState({'token': response.data.token}, this.get_data)
+                }
+            ).catch(
+            error => console.log(error)
+        )
+    }
+
+    logout() {
+        const cookie = new Cookies()
+        cookie.set('token', '')
+        this.setState({'token': ''}, this.get_data)
+    }
+
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users/')
-            .then(response => {
-                this.setState({users: response.data.results})
-            }).catch(error => console.log(error))
-        axios.get('http://127.0.0.1:8000/api/project/')
-            .then(response => {
-                this.setState({projects: response.data.results})
-            }).catch(error => console.log(error))
-        axios.get('http://127.0.0.1:8000/api/todo/')
-            .then(response => {
-                this.setState({todos: response.data.results})
-            }).catch(error => console.log(error))
+        this.get_token_from_storage()
+    }
+
+    divStyle = {
+        padding: "25px 0px",
+        textAlign: "left",
     }
 
     render() {
         return (
             <div className="App">
-                <div>
-                    <Menu/>
-                </div>
                 <HashRouter>
+                    <nav style={this.divStyle}>
+                        <ul>
+                            <li>
+                                {this.is_auth() ? <button onClick={() => this.logout()}>Logout</button> :
+                                    <Link to='/login'>Login</Link>}
+                            </li>
+                            <li>
+                                <Link to='/'>Users</Link>
+                            </li>
+                            <li>
+                                <Link to='/project'>Projects</Link>
+                            </li>
+                            <li>
+                                <Link to='/todo'>Todo</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
                     <Switch>
                         <Route exact path='/' component={() => <UserList users={this.state.users}/>}/>
+                        <Route exact path='/login' component={() => <LoginForm
+                            get_token={(login, password) => this.get_token(login, password)}/>}/>
                         <Route exact path='/project' component={() => <ProjectList projects={this.state.projects}/>}/>
                         <Route exact path='/todo' component={() => <TodoList todos={this.state.todos}/>}/>
                         <Route component={NotFound404}/>
@@ -63,20 +174,6 @@ class App extends React.Component {
 }
 
 
-class Menu extends React.Component {
-    divStyle = {
-        padding: "25px 0px",
-        textAlign: "left",
-    };
-
-    render() {
-        return (
-            <div style={this.divStyle}>
-                <MenuList/>
-            </div>
-        )
-    }
-}
 
 
 class Footer extends React.Component {
