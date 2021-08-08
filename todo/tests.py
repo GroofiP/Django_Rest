b894d8d3fbcbbf49a8d290755fac5f025fccae36
Groofi <varchikcreed@mail.ru>
2021-08-08 03:38:23 +0300

    Home_8

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
+from rest_framework import status
+from todo.models import Todo, Project
+from todo.views import TodoModelViewSet
+from rest_framework.test import APIRequestFactory, APITestCase, APIClient
 
-# Create your tests here.
+from users_app.models import User
+
+
+class TestTodoViewSet(TestCase):
+
+    def test_create_todo(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/todo/',
+                               {"headNote": "Hello12312213", "textNote": "Good luck!3123", "isActive": True,
+                                "project": 1, "user": 4})
+        view = TodoModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_detail_project(self):
+        todo = mixer.blend(Todo)
+        client = APIClient()
+        response = client.get(f'/api/project/{todo.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestTodosViewSet(APITestCase):
+
+    def test_get_list_todo(self):
+        response = self.client.get('/api/todo/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestProjectViewSet(TestCase):
+
+    def test_create_project(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/project/',
+                               {"headNote": "Hello12312213", "textNote": "Good luck!3123", "isActive": True,
+                                "project": 1, "user": 4})
+        view = TodoModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_detail_project(self):
+        project = mixer.blend(Project)
+        client = APIClient()
+        response = client.get(f'/api/project/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestProjectsViewSet(APITestCase):
+
+    def test_get_list_project(self):
+        response = self.client.get('/api/project/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- users_app/tests.py
+++ users_app/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APIClient, APITestCase
 
-# Create your tests here.
+from users_app.models import User
+from users_app.views import UserModelViewSet
+
+
+class TestUserViewSet(TestCase):
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/users/', {'first_name': 'Александр', 'last_name': "Пушкин", "username": "PushAlex",
+                                               "email": "pushala@mail.ru"})
+        view = UserModelViewSet.as_view({'post': 'update'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_detail(self):
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client = APIClient()
+        response = client.get(f'/api/users/{admin.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_edit_guest(self):
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client = APIClient()
+        response = client.put(f'/api/users/{admin.id}/',
+                              {'first_name': 'Александр', 'last_name': "Пушкин", "username": "PushAlex",
+                               "email": "pushala@mail.ru"})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_admin(self):
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client = APIClient()
+        user = User.objects.create(first_name="Александр", last_name="Пушкин", username="PushAlex",
+                                   email="pushala@mail.ru")
+        client.login(username='admin', password='admin123456')
+        response = client.put(f'/api/users/{user.id}/',
+                              {'first_name': 'Грин', 'last_name': "Пушкин", "username": "PushAlex",
+                               "email": "pushala@mail.ru"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user = User.objects.get(id=user.id)
+        self.assertEqual(user.first_name, 'Грин')
+        self.assertEqual(user.last_name, "Пушкин")
+        client.logout()
+
+
+class TestUsersViewSet(APITestCase):
+
+    def test_get_list(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
